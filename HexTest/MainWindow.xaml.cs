5ca766af7af8f416eb6adcdd6ac61a55f2f92d54
Antti Virkkunen <antti.virkkunen@gmail.com>
2018-03-15 18:30:22 +0200

    Add project files.

---
+++ HexTest/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HexTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+
+            Nullable<bool> result = dialog.ShowDialog();
+            if (result == true)
+            {
+                byte[] fileBytes = File.ReadAllBytes(dialog.FileName);
+                TextBoxHex.Text = BitConverter.ToString(fileBytes).Replace("-", " ");
+                TextBoxText.Text = Encoding.UTF8.GetString(fileBytes);
+            }
+            
+        }
+    }
+}
---
+++ PngMeta/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PngMeta
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.Filter = "PNG images|*.png";
+
+            Nullable<bool> result = dialog.ShowDialog();
+            if (result == true)
+            {
+                myImage.Source = new BitmapImage(new Uri(dialog.FileName));
+
+                byte[] fileBytes = File.ReadAllBytes(dialog.FileName);
+                //myTextBox.Text = BitConverter.ToString(fileBytes).Replace("-", " ");
+                myTextBox.Text = Encoding.UTF8.GetString(fileBytes);
+            }
+
+        }
+    }
+}
