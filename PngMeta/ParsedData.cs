89566a556a471fa31f0b7df7d35696f6e91f65c9
k2705 <k2705@student.jamk.fi>
2018-04-25 20:23:30 +0300

    fix: text fields are only supposed to have one key-value pair

--- PngMeta/ParsedData.cs
+++ PngMeta/ParsedData.cs
@@ -77,49 +77,41 @@ namespace PngMeta
     public class ParsedTEXT : ParsedChunkData
     {
         private Encoding latin1 = Encoding.GetEncoding("ISO-8859-1"); //Latin-1 charset
-        public List<StringPair> TextData { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
 
         public ParsedTEXT(byte[] data)
         {
-            TextData = new List<StringPair>();
             StringBuilder sbKey = new StringBuilder();
             StringBuilder sbValue = new StringBuilder();
 
             int i = 0;
-            while (i < data.Length)
+
+            //keyword is supposed to be 1-79 characters but better not assume
+            for (; i < data.Length && data[i] != 0x00; i++) //until null character or end of array
             {
-                //keyword is supposed to be 1-79 characters but better not assume
-                for (; i < data.Length && data[i] != 0x00; i++) //until null character or end of array
-                {
-                    sbKey.Append(latin1.GetChars(data, i, 1));
-                }
-                //found null (or data ended prematurely)
-                i++;
-                //data can be any length, including zero
-                for (; i < data.Length && data[i] != 0x00; i++)
-                {
-                    sbValue.Append(latin1.GetChars(data, i, 1));
-                }
-                i++;
-                //another null or end of data
-                TextData.Add(new StringPair(sbKey.ToString(), sbValue.ToString()));
-                sbKey.Clear();
-                sbValue.Clear();
+                sbKey.Append(latin1.GetChars(data, i, 1));
             }
-            
+            //found null (or data ended prematurely)
+            i++;
+            //data can be any length, including zero, not null-terminated
+            for (; i < data.Length; i++)
+            {
+                sbValue.Append(latin1.GetChars(data, i, 1));
+            }
+
+            Key = sbKey.ToString();
+            Value = sbValue.ToString();
+
+
         }
 
         public override byte[] GetBytes()
         {
             List<byte> bytes = new List<byte>();
-            foreach (StringPair k in TextData)
-            {
-                bytes.AddRange(latin1.GetBytes(k.Key));
-                bytes.Add(0x00);
-                bytes.AddRange(latin1.GetBytes(k.Value));
-                bytes.Add(0x00);
-            }
-            bytes.RemoveAt(bytes.Count - 1); // loop adds one extra null byte at the end
+            bytes.AddRange(latin1.GetBytes(Key));
+            bytes.Add(0x00);
+            bytes.AddRange(latin1.GetBytes(Value));
             return bytes.ToArray();
         }
     }
