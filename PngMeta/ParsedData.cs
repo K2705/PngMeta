6f9b5a478597c3c3573ff864f9989d2c28848434
k2705 <k2705@student.jamk.fi>
2018-05-02 20:17:33 +0300

    remove unused class

--- PngMeta/ParsedData.cs
+++ PngMeta/ParsedData.cs
@@ -293,15 +293,4 @@ namespace PngMeta
         }
     }
 
-
-    public class StringPair
-    {
-        public string Key { get; set; }
-        public string Value { get; set; }
-        public StringPair(string key, string value)
-        {
-            this.Key = key;
-            this.Value = value;
-        }
-    }
 }
