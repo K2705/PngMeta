b77a5d5a7ac1493ef5c66f78f3826503927bbefd
k2705 <k2705@student.jamk.fi>
2018-05-02 20:41:04 +0300

    Revert "Remove static class ImageData, move only method to ImageWrapper"
    This reverts commit d9fc857119da069ef5bd785f4e718fe9fc8155fc.

--- PngMeta/ImageWrapper.cs
+++ PngMeta/ImageWrapper.cs
@@ -31,7 +31,7 @@ namespace PngMeta
                 Image.EndInit();
             }
             FileBytes = File.ReadAllBytes(path);
-            FileChunks = GetChunks(FileBytes);
+            FileChunks = ImageData.GetChunks(FileBytes);
         }
         
         /// <summary>
@@ -58,45 +58,6 @@ namespace PngMeta
         internal void SaveImage(string path, byte[] buffer)
         {
             File.WriteAllBytes(path, buffer);
-        }
-
-        private static List<PngDataChunk> GetChunks(byte[] imageBytes)
-        {
-            // Check for the eight-byte PNG signature
-            byte[] pngHeader = { 137, 80, 78, 71, 13, 10, 26, 10, };
-            for (int i = 0; i < 8; i++)
-            {
-                if (pngHeader[i] != imageBytes[i]) throw new ArgumentException("No PNG signature found");
-            }
-
-            List<PngDataChunk> chunks = new List<PngDataChunk>();
-            int k = 8;
-
-            while (true)
-            {
-                // get the data
-                UInt32 chunkLength = ByteUtils.ToUInt32(imageBytes, k);
-                k += 4;
-                ChunkType chunkType = new ChunkType(imageBytes, k);
-                k += 4;
-                byte[] chunkData = new byte[chunkLength];
-                for (int i = 0; i < chunkLength; i++, k++)
-                {
-                    chunkData[i] = imageBytes[k];
-                }
-                UInt32 chunkCRC = ByteUtils.ToUInt32(imageBytes, k);
-                k += 4;
-
-                // create chunk from data and add it to list
-                PngDataChunk dataChunk = new PngDataChunk(chunkLength, chunkType, chunkData, chunkCRC);
-                chunks.Add(dataChunk);
-
-                // stop once we hit the IEND
-                if (chunkType.ToString() == "IEND") break;
-                // TODO: proper error handling if IEND is never found
-            }
-
-            return chunks;
         }
     }
 }
